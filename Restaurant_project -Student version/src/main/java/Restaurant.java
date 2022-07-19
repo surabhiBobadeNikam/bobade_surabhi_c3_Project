8a39e3137a4f1995995fe77fe12db987c56e37ec
sbobade <surabhi.bobade@yahoo.in>
2022-07-19 17:11:11 +0530

    Part 2: Solution

--- Restaurant_project -Student version/src/main/java/Restaurant.java
+++ Restaurant_project -Student version/src/main/java/Restaurant.java
@@ -18,15 +18,18 @@ public class Restaurant {
     }
 
     public boolean isRestaurantOpen() {
-        return true;
-        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+        LocalTime currentTime = getCurrentTime();
+        return openingTime.isBefore(currentTime) && closingTime.isAfter(currentTime);
     }
 
     public LocalTime getCurrentTime(){ return  LocalTime.now(); }
 
     public List<Item> getMenu() {
-        return null;
-        //DELETE ABOVE RETURN STATEMENT AND WRITE CODE HERE
+        List<Item> menuList= new ArrayList<>();
+        for(Item item: menu) {
+            menuList.add(item);
+        }
+        return menuList;
     }
 
     private Item findItemByName(String itemName){
